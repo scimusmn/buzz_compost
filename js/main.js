c31728906125330f97b090918767cdc918145f8f
bryan kennedy <bkennedy@smm.org>
2014-05-13 10:50:13 -0500

    Adding js to return to home when video ends

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+/**
+ * Return to the homepage when the video ends
+ */
+videojs("example_video_1").ready(function(){
+  this.on("ended", function(){
+      window.location = "index.html";
+  });
+});
